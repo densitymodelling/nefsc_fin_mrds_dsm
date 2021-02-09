0f5dbc2be60c759369d91e9ffe66d4acfbd2f1e8
David Lawrence Miller <github@ninepointeightone.net>
2021-02-09 19:30:16 +0000

    correct various data issues, now requires dsm 2.3.1.9012 or later

--- data_processing.R
+++ data_processing.R
@@ -6,16 +6,24 @@
 # Load data files
 # Sightings data formatted for mrds
 mrds_Sightings <- read.csv("data/Fin_Sightings_mrds_for_dsm.csv")
+# segment data
 Fin_Whale_Data <- read.csv("data/Final_Fin_Whale_Data_for_dsm.csv")
 
+
+# rename columns, data munging
 # rescale distances
 mrds_Sightings$distance <- mrds_Sightings$distance/1000
+# remove -9999 observer
+mrds_Sightings <- subset(mrds_Sightings, observer != -9999)
 
 # get column names
 mrds_Sightings$Sample.Label <- mrds_Sightings$segment
 mrds_Sightings$segment <- NULL
 Fin_Whale_Data$Sample.Label <- Fin_Whale_Data$segment
 Fin_Whale_Data$segment <- NULL
+
+Fin_Whale_Data$Sample.Label <- paste0(Fin_Whale_Data$Survey, "-", Fin_Whale_Data$Sample.Label)
+mrds_Sightings$Sample.Label <- paste0(mrds_Sightings$survey, "-", mrds_Sightings$Sample.Label)
 
 # identifier for which detectioin function this effort is for
 Fin_Whale_Data$ddfobj <- Fin_Whale_Data$Survey
@@ -32,9 +40,11 @@ Fin_Whale_Data$Beaufort <- NULL
 #   1   fin whale
 #   2   fin or sei
 #   3   sei whale
+# we only want to fit the spatial model to the fin data only
 fin_segs <- subset(Fin_Whale_Data, Species_Hab_Num==1)
 fin_segs <- fin_segs[, c("Sample.Label", "Effort", "DIST125", "DEPTH",
-                         "DIST2SHORE", "SST", "ddfobj", "SUBJ_WAVG", "beaufort")]
+                         "DIST2SHORE", "SST", "ddfobj", "SUBJ_WAVG",
+                         "beaufort")]
 
 # truncation distances
 w_ship <- 6
@@ -49,21 +59,37 @@ mrds_Sightings <- mrds_Sightings[, c("object", "observer", "detected",
 
 # shipboard survey data
 ship_detections <- subset(mrds_Sightings,
-                          survey==1 & distance>-1 & distance < w_ship)
+                          survey == 1 &          # ship detections
+                          distance <= w_ship)  # within truncation
 
 # aerial survey data
 # all sightings from the front team
 plane_detections <- subset(mrds_Sightings,
-                           survey==2 & distance>-1 & distance < w_plane &
-                           observer==1 & detected==1)
+                           survey == 2 &          # plane detections
+                           distance <= w_plane &  # within truncation
+                           observer == 1 &        # front team only
+                           detected == 1)         # and detected!
 
-# we're going to fit detection functions with all detections (fin, fin/sei, sei)
+
+
+## we're going to fit detection functions with all detections (fin, fin/sei, sei)
 # but only build the spatial model with fin detections
-# build observation data.frame
-fin_obs <- rbind(ship_detections, plane_detections)
-# filter only fin detections
+# build observation data.frame that reflects this
+fin_obs <- mrds_Sightings
+
+## filter only fin detections
 fin_obs <- fin_obs[fin_obs$Species_Hab_Num == 1, ]
 fin_obs$Species_Hab_Num <- NULL
+# only want one of each observation in the obs frame
+fin_obs <- fin_obs[!duplicated(fin_obs$object), ]
+fin_obs$ddfobj <- fin_obs$survey
+## only get observations within truncation
+#fin_obs <- subset(fin_obs, (ddfobj == 1 & distance < w_ship) |
+#                           (ddfobj == 2 & distance < w_plane))
+
+# select only the columns we want
+fin_obs <- fin_obs[, c("object", "Sample.Label", "ddfobj", "size", "distance")]
+
 
 
 # Import data set with values for all grid cells
